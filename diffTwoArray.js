21a00e933315477fa10dff3ef10cdec9eab35a7d
Heith <Heith.Robbins@gmail.com>
2020-09-23 13:34:42 -0600

    worked on difftwoArry

--- diffTwoArray.js
+++ diffTwoArray.js
@@ -4,21 +4,21 @@
 // if it not found add to not-found array
 // return not_found array
 
-function diffArray(arr1, arr2) {
-  return arr1
-    .concat(arr2)
-    .filter((item) => !arr1.includes(item) || !arr2.includes(item));
-}
+// function diffArray(arr1, arr2) {
+//   return arr1
+//     .concat(arr2)
+//     .filter((item) => !arr1.includes(item) || !arr2.includes(item));
+// }
 
-console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+// console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
-function diffArray(arr1, arr2) {
-  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+// function diffArray(arr1, arr2) {
+//   return [...diff(arr1, arr2), ...diff(arr2, arr1)];
 
-  function diff(a, b) {
-    return a.filter((item) => b.indexOf(item) === -1);
-  }
-}
+//   function diff(a, b) {
+//     return a.filter((item) => b.indexOf(item) === -1);
+//   }
+// }
 
 function diffArray(arr1, arr2) {
   var newArr = [];
@@ -39,4 +39,4 @@ function diffArray(arr1, arr2) {
   return newArr;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]));
