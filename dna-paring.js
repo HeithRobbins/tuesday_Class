2fe4c69a3f7eb500406c0314738ecdb200b1987f
Heith <Heith.Robbins@gmail.com>
2020-10-11 11:08:54 -0600

    penguin

--- dna-paring.js
+++ dna-paring.js
@@ -1,43 +1,43 @@
 //www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/dna-pairing
 
-https: function pairElement(str) {
-  return str;
-}
+// https: function pairElement(str) {
+//   return str;
+// }
 
-pairElement("GCG");
+// pairElement("GCG");
 
-function pairElement(str) {
-  // Return each strand as an array of two elements, the original and the pair.
-  var paired = [];
+// function pairElement(str) {
+//   // Return each strand as an array of two elements, the original and the pair.
+//   var paired = [];
 
-  // Function to check with strand to pair.
-  var search = function (char) {
-    switch (char) {
-      case "A":
-        paired.push(["A", "T"]);
-        break;
-      case "T":
-        paired.push(["T", "A"]);
-        break;
-      case "C":
-        paired.push(["C", "G"]);
-        break;
-      case "G":
-        paired.push(["G", "C"]);
-        break;
-    }
-  };
+//   // Function to check with strand to pair.
+//   var search = function (char) {
+//     switch (char) {
+//       case "A":
+//         paired.push(["A", "T"]);
+//         break;
+//       case "T":
+//         paired.push(["T", "A"]);
+//         break;
+//       case "C":
+//         paired.push(["C", "G"]);
+//         break;
+//       case "G":
+//         paired.push(["G", "C"]);
+//         break;
+//     }
+//   };
 
-  // Loops through the input and pair.
-  for (var i = 0; i < str.length; i++) {
-    search(str[i]);
-  }
+//   // Loops through the input and pair.
+//   for (var i = 0; i < str.length; i++) {
+//     search(str[i]);
+//   }
 
-  return paired;
-}
+//   return paired;
+// }
 
-// test here
-console.log(pairElement("GCG"));
+// // test here
+// console.log(pairElement("GCG"));
 
 //change it to a dictionnary in stand of a swicth case
 
